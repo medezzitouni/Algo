8c37ab00d5ac8d65eadcbb71b6826f2382679aaa
methe ezzitouni <methe.medpd@gmail.com>
2023-04-05 02:32:22 +0000

    added MostOccChar in Java

--- Java/MostOccChar.java
+++ Java/MostOccChar.java
@@ -3,19 +3,19 @@ import java.util.HashMap;
 
 class MostOccChar {
 
-    public static void main(String args[]){
-      
-      System.out.println(mostOccChar(args[0]));
+    public static void main(String args[]) {
+
+        System.out.println(mostOccChar(args[0]));
     }
 
-    public static Character mostOccChar(String phrase){
-        
+    public static Character mostOccChar(String phrase) {
+
         HashMap<Character, Integer> chars = new HashMap<Character, Integer>();
         Character mostOcc = null;
 
         chars.put(mostOcc, 0);
 
-        for(char s: phrase.toCharArray()){
+        for (char s : phrase.toCharArray()) {
             chars.put(s, chars.containsKey(s) ? chars.get(s) + 1 : 1);
             mostOcc = chars.get(s) > chars.get(mostOcc) ? s : mostOcc;
         }
